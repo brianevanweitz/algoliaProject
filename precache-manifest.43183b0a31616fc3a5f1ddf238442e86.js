eed7c4757a8b928c1450ab0e096245d2ca7ea0fd
brianevanweitz <brianweitz@Brians-MacBook-Pro-2.local>
2020-10-24 19:27:33 -0400

    Updates

---
+++ precache-manifest.43183b0a31616fc3a5f1ddf238442e86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "392e13c2c3022e7a0844",
+    "url": "/algoliaProject/static/css/main.17028b5e.chunk.css"
+  },
+  {
+    "revision": "392e13c2c3022e7a0844",
+    "url": "/algoliaProject/static/js/main.392e13c2.chunk.js"
+  },
+  {
+    "revision": "8e44dc9c7707bdf37e18",
+    "url": "/algoliaProject/static/js/1.8e44dc9c.chunk.js"
+  },
+  {
+    "revision": "e1f788d00b0e8fa1e0eb",
+    "url": "/algoliaProject/static/js/runtime~main.e1f788d0.js"
+  },
+  {
+    "revision": "aa7d02ec880c9bf0e92b479f775a3e85",
+    "url": "/algoliaProject/static/media/field3.aa7d02ec.jpg"
+  },
+  {
+    "revision": "aee1d17f65b8e58326b3e10e94582ba1",
+    "url": "/algoliaProject/index.html"
+  }
+];
