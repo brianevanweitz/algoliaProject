40a789e5016c62381ab6ce9148ce2b1a5bd7864b
brianevanweitz <brianweitz@Brians-MacBook-Pro-2.local>
2020-10-24 12:05:51 -0400

    more data, header styling

--- football-app/src/App.js
+++ football-app/src/App.js
@@ -8,7 +8,6 @@ import {
   Highlight,
   ClearRefinements,
   RefinementList,
-  Configure,
 } from 'react-instantsearch-dom';
 import PropTypes from 'prop-types';
 import './App.css';
@@ -24,14 +23,9 @@ class App extends Component {
       <div>
         <header className="header">
           <h1 className="header-title">
-            <a href="/">NFL Player Statistics</a>
+            <a href="/">NFL Player Database</a>
           </h1>
-          <p className="header-subtitle">
-            using{' '}
-            <a href="https://github.com/algolia/react-instantsearch">
-              React InstantSearch
-            </a>
-          </p>
+          <img className='fb-clipart' src='https://lh3.googleusercontent.com/proxy/jFbNaySUwBkAFgcBPakoLhJ8K4tNkbKXcuvv1WuU4b_zLEoYBBjZnXFkq-mWDq9JTDGEVQkK3OPGTHI-8ZT1xXpS1HbzFGc' />
         </header>
         <div className="container">
           <InstantSearch searchClient={searchClient} indexName="Football">
@@ -62,6 +56,12 @@ class App extends Component {
   }
 }
 
+function getAge(birthdate) {
+  const birthYear = parseInt(birthdate.substr(0, 4));
+  const currentYear = new Date().getFullYear();
+  return (currentYear - birthYear)
+}
+
 function Hit(props) {
   return (
     <div className='player-card'>
@@ -70,9 +70,12 @@ function Hit(props) {
           {props.hit.position}
         </h1>
       </div>
-      <h3>
+      <h2>
         <Highlight attribute="name" hit={props.hit} />
-      </h3>
+      </h2>
+      {props.hit.current_team ? (<p>{props.hit.current_team}</p>) : (<p>No current team</p>)}
+      {props.hit.draft_round ? (<p>Round Drafted: {props.hit.draft_round}</p>) : (<p>No draft data</p>)}
+      {props.hit.birth_date ? (<p>Age: {getAge(props.hit.birth_date)}</p>) : (<p>No age data</p>)}
     </div>
   );
 }
