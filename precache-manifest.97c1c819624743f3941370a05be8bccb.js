3171823bd5a304ad21549a7efcc668ffad241ea5
brianevanweitz <brianweitz@Brians-MacBook-Pro-2.local>
2020-10-24 19:29:38 -0400

    Updates

---
+++ precache-manifest.97c1c819624743f3941370a05be8bccb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "acff23ae467b1f7bcea0",
+    "url": "/footballApp/static/css/main.ca75174d.chunk.css"
+  },
+  {
+    "revision": "acff23ae467b1f7bcea0",
+    "url": "/footballApp/static/js/main.acff23ae.chunk.js"
+  },
+  {
+    "revision": "848d9ff7f9230998dac2",
+    "url": "/footballApp/static/js/1.848d9ff7.chunk.js"
+  },
+  {
+    "revision": "76419a087b875cec9cd0",
+    "url": "/footballApp/static/js/runtime~main.76419a08.js"
+  },
+  {
+    "revision": "aa7d02ec880c9bf0e92b479f775a3e85",
+    "url": "/footballApp/static/media/field3.aa7d02ec.jpg"
+  },
+  {
+    "revision": "6464c917c5e622b574f1230619b4fc83",
+    "url": "/footballApp/index.html"
+  }
+];
